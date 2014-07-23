b03a39f5d97b8f402f5e2d644856b12d0b2dc1ad
Caleb Albritton <ithinkincode@gmail.com>
2014-07-23 16:08:54 -0500

    added blocking

--- lib/eventmapper.rb
+++ lib/eventmapper.rb
@@ -1,6 +1,19 @@
 require 'eventmapper/version'
 
 module EventMapper
+
+  def set_event_blocking(bool)
+    @event_blocks = bool
+  end
+
+  def event_blocks?
+    if @event_blocks.nil?
+      @event_blocks = true
+    else
+      @event_blocks
+    end
+  end
+
   def on(event, &block)
     if @events.nil?
       @events = { event => [block] }
@@ -12,13 +25,25 @@ module EventMapper
   end
 
   def fire(event, *args)
+    if @event_blocks.nil?
+      @event_blocks = true
+    end
+
     if args.empty?
       @events[event].each do |proc|
-        Thread.new { proc.call }
+        if @event_blocks
+          proc.call
+        else
+          Thread.new { proc.call }
+        end
       end
     else
       @events[event].each do |proc|
-        Thread.new { proc.call *args }
+        if @event_blocks
+          proc.call *args
+        else
+          Thread.new { proc.call *args }
+        end
       end
     end
   end
--- lib/eventmapper/version.rb
+++ lib/eventmapper/version.rb
@@ -1,3 +1,3 @@
 module EventMapper
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
